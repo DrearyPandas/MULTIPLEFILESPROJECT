7033ed2f35e2adfa5cff443c1c156270fc2b3ad6
JOHN MILLER <JOHMILLE@uat.edu>
2018-11-29 14:30:30 -0700

    spacebar

--- MULTIPLEFILESPROJECT/MAIN.cpp
+++ MULTIPLEFILESPROJECT/MAIN.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 	bool gameCont = true; // repeats the while loop while its true
-	tutorial(); // calls the tutorial function instructing you on how to play
+	tutorial(); // calls the tutorial function instructing you on how to play 
 	while (gameCont)
 	{
 		gameCont = gamePlay(); // calls the gameplay function
